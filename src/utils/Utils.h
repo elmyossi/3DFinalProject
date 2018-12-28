45fdc55b04d12815b3de2d98d921807e479a07e4
Yossi Elman <yossi.elman@gmail.com>
2018-12-28 12:45:30 +0200

    change of folder structure + start Jaap's core

--- src/utils/Utils.h
+++ src/utils/Utils.h
@@ -10,17 +10,24 @@
 #include <igl/opengl/glfw/imgui/ImGuiHelpers.h>
 #include <igl/opengl/glfw/imgui/ImGuiTraits.h>
 #include <iostream>
+#include <cmath>
 
+const double EulerConstant = std::exp(1.0);
 typedef Eigen::Matrix<double, 1, 3> RowVector3;
+
 
 class Utils {
 
 public:
     static RowVector3 calculateCenterOfMass(Eigen::MatrixXd V, Eigen::MatrixXi F);
+    static RowVector3 calculateCenterOfMassInside(Eigen::MatrixXd V, Eigen::MatrixXi F);
+
 
 private:
     static RowVector3 getCenterOfTriangle(RowVector3 p1, RowVector3 p2, RowVector3 p3);
     static double getAreaOfTriangle(RowVector3 v1, RowVector3 v2, RowVector3 v3);
+    static RowVector3 Centroid(RowVector3 p1, RowVector3 p2, RowVector3 p3, RowVector3 p4);
+    static double SignedVolume(RowVector3 p1, RowVector3 p2, RowVector3 p3, RowVector3 p4);
 
 };
 
