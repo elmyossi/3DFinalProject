7ee2ee949c920fe3abea00fcfa9e84ee9887a9a8
michaelozerikenshoo <michael.ozeri@kenshoo.com>
2018-12-29 11:39:04 +0200

    added argmin center of japSphere. not working good yet.

--- src/JapSphere/japSphere.cpp
+++ src/JapSphere/japSphere.cpp
@@ -11,7 +11,8 @@ double japSphere::getMinOutOfMesh(Eigen::MatrixXd meshPointsS, RowVector3 p) {
     double temp;
     for (int i = 1; i < meshPointsS.rows(); i++) {
         RowVector3 q = meshPointsS.row(i);
-        if ((temp = calcDistance(q, p)) < minVal) {
+        temp = calcDistance(q, p);
+        if (temp < minVal) {
             minVal = temp;
             argMin = q;
         }
@@ -23,13 +24,15 @@ double japSphere::calcDistance(RowVector3 q, RowVector3 p) {
     return sqrt(pow((q[0] - p[0]), 2) + pow((q[1] - p[1]), 2) + pow((q[2] - p[2]), 2));
 }
 
-RowVector3 japSphere::calculateCenterOfSphere(Eigen::MatrixXd meshPointsS, Eigen::MatrixXd meshPointsInnerS, int numberOfRowsInner) {
+RowVector3 japSphere::calculateCenterOfSphere(Eigen::MatrixXd meshPointsS, Eigen::MatrixXd meshPointsInnerS,
+                                              int numberOfRowsInner) {
     RowVector3 argMax = meshPointsInnerS.row(0);
     double maxVal = getMinOutOfMesh(meshPointsS, argMax);
     double temp;
     for (int i = 1; i < numberOfRowsInner; i++) {
         RowVector3 p = meshPointsInnerS.row(i);
-        if ((temp = getMinOutOfMesh(meshPointsS, p)) > maxVal) {
+        temp = getMinOutOfMesh(meshPointsS, p);
+        if (temp > maxVal) {
             argMax = p;
             maxVal = temp;
         }
